a730e7ea533ab7b7e56594d16e5b3c3fb65cca8d
User <user@Users-MacBook-Pro.local>
2024-07-29 00:18:04 -0400

    Chef activity create

---
+++ app/src/main/java/com/example/chef_in_home/ChefActivity.java
@@ -0,0 +1,14 @@
+package com.example.chef_in_home;
+
+import androidx.appcompat.app.AppCompatActivity
+
+public class ChefActivity extends AppCompatActivity {
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_chef);
+
+    }
+
+
+}
